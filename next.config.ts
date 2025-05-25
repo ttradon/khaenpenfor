594c886e4a3662bc74f4884c2d9c7f14d8ca33df
ttradon <ttradon0123@gmail.com>
2025-05-25 19:39:46 +0700

    final

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/api/:path*", // ทุก route ที่ขึ้นต้นด้วย /api
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" }, // หรือใส่ origin ที่เฉพาะเจาะจง
+          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
